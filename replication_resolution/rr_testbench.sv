3f5a2359fcf8689594eed47925d4a2ac54d37d71
Kevin Townsend <kt@krtownsend.com>
2017-01-29 04:00:53 -0800

    .

--- replication_resolution/rr_testbench.sv
+++ replication_resolution/rr_testbench.sv
@@ -1,6 +1,15 @@
 module rr_testbench;
+// TODO: clock
+//signal i;
+//reg clk;
+//reg [0 : 3] wr;
+//wire [5 : 0] wr_addr [0 : INPUTS];
+
+//timstamp_rr dut(clk, wr, wr_addr);
+
 initial begin
     $display("hello world");
+
     $finish;
 end
 endmodule
--- replication_resolution/timestamp_rr.sv
+++ replication_resolution/timestamp_rr.sv
@@ -1,12 +1,17 @@
-module timestamp_rr {
+module timestamp_rr;
+`include "common/common.vh"
     parameter WIDTH = 64;
     parameter DEPTH = 512;
     parameter INPUTS = 4;
     parameter OUTPUTS = 4;
     parameter TIME_BITS = 16;
-    parameter ADDR_WIDTH = BIT_WIDTH(DEPTH - 1);
+    parameter ADDR_WIDTH = bit_count(DEPTH - 1);
     input clk;
     input [0 : INPUTS - 1] wr;
-    output [ADDR_WIDTH - 1 : 0] wr_addr [0 : INPUTS];
+    input [ADDR_WIDTH - 1 : 0] wr_addr [0 : INPUTS - 1];
+
+    always @(posedge clk) begin
+        //wr_addr <= wr;
+    end
     // TODO: complete
-}
+endmodule
